4e053296bf5a4b3ac1b1d46ce2c4cd0344018c0f
IzatheGreat15 <20100869@usc.edu.ph>
2022-10-18 00:22:59 +0800

    insert node in skiplist

--- skipList.c
+++ skipList.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <time.h>
 
 #define MAX_LEVEL 5
 typedef struct node{
     int data;             // contains the actual key value
     int level;            // contains the current level of the node
-    struct node **next;   // array of pointers
+    struct node **prev;   // array of pointers to previous nodes
+    struct node **next;   // array of pointers to next nodes
 }*Node;
 
 typedef struct{
@@ -28,26 +30,106 @@ Node createNode(int data, int level_size){
     /* pass the data and level, and create array of pointers */
     newNode->data = data;
     newNode->level = level_size;
+    // newNode->prev = (Node*)calloc(newNode->level, sizeof(Node));
     newNode->next = (Node*)calloc(newNode->level, sizeof(Node));
 
     return newNode;
 }
 
+/**
+ * @brief initialize an empty skiplist
+ * 
+ * @param list 
+ */
 void initializeList(Skiplist *list){
     /* create the first node if the list (negative infinity) */
     list->head = createNode(INT_MIN, list->maxLevel);
 
     /* create the last node of the list (positive infinity) */
     Node last = createNode(INT_MAX, list->maxLevel);
-    /* array of pointers of the head should all point to the last node */
+    /**
+     * next pointers of head => last node
+     * prev pointers of last => head
+     */
     int x;
     for(x = 0; x < list->head->level; x++){
         list->head->next[x] = last;
+        // last->prev[x] = list->head;
     }
+}
+
+/**
+ * @brief flip a coin
+ * 
+ * @return int 1 => head; 0 => tail
+ */
+int coinFlip(){
+    return rand() % 2;
+}
+
+/**
+ * @brief inserting a new node to the skiplist
+ * 
+ * @param head Head node
+ * @param key actual key value to insert
+ * @param level optional
+ */
+void insertNode(Node head, int key, int level){
+    Node trav = head;
+
+    // if(level < 0)
+    //     return;
+
+    /* traverse to the right level */
+    for(level = trav->level - 1; key < trav->next[level]->data && level > 0; level--){
+        /* no duplicates */
+        if(key == trav->next[level]->data)
+            return;
+
+        /* move trav to next checkpoint */
+        if(key > trav->next[level - 1]->data)
+            trav = trav->next[level - 1];
+    }
+    // if(key < trav->next[level]->data){
+    //     insertNode(trav, key, level - 1);
+    // }else if(key > trav->next[level]->data){
+    //     insertNode(trav->next[level], key, level);
+    // }
+    
+    /* determine level size of new node using coin flip */
+    int level_size = 1, coin;
+    do{
+        coin = coinFlip();
+        level_size += coin;
+    }while(coin && level_size < MAX_LEVEL);
+
+    Node newNode = createNode(key, level_size);
+    /* insert node */
+    int x;
+    for(x = 0; x < level_size; x++){
+        if(x == trav->level)
+            trav = trav->prev[x - 1];
+
+        newNode->next[x] = trav->next[x];
+        newNode->prev[x] = newNode->next[x]->prev[x];
+        newNode->next[x]->prev[x] = newNode;
+        trav->next[x] = newNode;
+    }
+}
+
+Node searchNode(Node head, int key){
+    Node foundNode = NULL;
+    
+
+    return foundNode;
 }
 
 void main(){
     Skiplist list = { NULL, MAX_LEVEL };
 
+    srand(time(NULL));
+
     initializeList(&list);
+    insertNode(list.head, 56, list.maxLevel - 1);
+    insertNode(list.head, 60, list.maxLevel - 1);
 }
